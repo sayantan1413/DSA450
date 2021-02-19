300f1d16a600c37be56445db6ea7a59661dd7c9c
sayantan1413 <sayantanbose2001@gmail.com>
2021-02-19 20:05:54 +0530

    Added programs on Arrays

---
+++ Arrays/Sort012withoutsorting/sort012.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int a[n];
+        int count_0 = 0, count_1 = 0, count_2 = 0;
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+            if (a[i] == 0)
+                count_0++;
+            else if (a[i] == 1)
+                count_1++;
+            else if (a[i] == 2)
+                count_2++;
+        }
+        for (int i = 0; i < n; i++)
+        {
+            if (count_0 > 0)
+            {
+                a[i] = 0;
+                count_0--;
+            }
+            else if (count_1 > 0)
+            {
+                a[i] = 1;
+                count_1--;
+            }
+            else if (count_2 > 0)
+            {
+                a[i] = 2;
+                count_2--;
+            }
+        }
+        for (int i = 0; i < n; i++)
+        {
+            cout << a[i] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
